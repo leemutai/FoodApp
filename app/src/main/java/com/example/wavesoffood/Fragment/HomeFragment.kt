35dbdda9164047c4b95f9f793448255735241e00
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-16 09:39:19 +0300

    added menu, navigation and its fragments

--- app/src/main/java/com/example/wavesoffood/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/HomeFragment.kt
@@ -19,6 +19,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 }
--- app/src/main/java/com/example/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.wavesoffood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val navController: NavController by lazy {
+        findNavController(R.id.fragmentContainerView4)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView4)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
