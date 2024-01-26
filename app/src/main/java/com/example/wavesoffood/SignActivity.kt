721dde002b8742a756afa49977b128d857c88801
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-26 11:30:59 +0300

    implemented   sign in

--- app/src/main/java/com/example/wavesoffood/SignActivity.kt
+++ app/src/main/java/com/example/wavesoffood/SignActivity.kt
@@ -3,18 +3,111 @@ package com.example.wavesoffood
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.wavesoffood.databinding.ActivitySignBinding
+import com.example.wavesoffood.model.UserModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class SignActivity : AppCompatActivity() {
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var username: String
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private lateinit var googleSignInClient: GoogleSignInClient
     private val binding: ActivitySignBinding by lazy {
         ActivitySignBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        //initialize firebase auth
+        auth = Firebase.auth
+        //initialize firebase database
+        database = Firebase.database.reference
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions)
+
+        binding.createAccountButton.setOnClickListener {
+            username = binding.userName.text.toString().trim()
+            email = binding.emailAddress.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            if (email.isEmpty() || password.isBlank() || username.isBlank()) {
+                Toast.makeText(this, "Please Fill all the details", Toast.LENGTH_SHORT).show()
+            } else {
+                createAccount(email, password)
+            }
+
+        }
         binding.alreadyhavebutton.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Account created succesfully", Toast.LENGTH_SHORT).show()
+                saveUserData()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Account creation failed", Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createAccount: Failure", task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        //retrieve data from input field
+        username = binding.userName.text.toString()
+        email = binding.emailAddress.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user = UserModel(username,email,password)
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        //save data to firebase database
+        database.child("user").child(userId).setValue(user)
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/wavesoffood/adaptar/CartAdapter.kt
+++ app/src/main/java/com/example/wavesoffood/adaptar/CartAdapter.kt
@@ -6,58 +6,68 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wavesoffood.databinding.CartItemBinding
 
-class CartAdapter(private val CartItems:MutableList<String>, private val CartItemPrice:MutableList<String>, private val CartImage:MutableList<Int>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
-private val itemQuantities = IntArray(CartItems.size){1}
+class CartAdapter(
+    private val CartItems: MutableList<String>,
+    private val CartItemPrice: MutableList<String>,
+    private val CartImage: MutableList<Int>,
+) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+    private val itemQuantities = IntArray(CartItems.size) { 1 }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
-        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CartViewHolder(binding)
     }
+
     override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
-         holder.bind(position)
+        holder.bind(position)
     }
+
     override fun getItemCount(): Int = CartItems.size
-    inner class CartViewHolder(private val binding: CartItemBinding) :RecyclerView.ViewHolder(binding.root) {
+    inner class CartViewHolder(private val binding: CartItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemQuantities[position]
-                cartFoodName.text=CartItems[position]
-                cartItemPrice.text=CartItemPrice[position]
+                cartFoodName.text = CartItems[position]
+                cartItemPrice.text = CartItemPrice[position]
                 cartImage.setImageResource(CartImage[position])
-                catItemQuantity.text=quantity.toString()
+                catItemQuantity.text = quantity.toString()
 
-                minusbutton.setOnClickListener{
+                minusbutton.setOnClickListener {
                     decreaseQuantity(position)
                 }
-                plusbutton.setOnClickListener{
-increaseQuantity(position)
+                plusbutton.setOnClickListener {
+                    increaseQuantity(position)
                 }
-                deleteButton.setOnClickListener{
-               val itemPosition = adapterPosition
-                    if ( itemPosition != RecyclerView.NO_POSITION){
+                deleteButton.setOnClickListener {
+                    val itemPosition = adapterPosition
+                    if (itemPosition != RecyclerView.NO_POSITION) {
                         deleteItem(itemPosition)
                     }
                 }
             }
         }
-        private  fun increaseQuantity(position: Int){
-            if (itemQuantities[position]<10){
+
+        private fun increaseQuantity(position: Int) {
+            if (itemQuantities[position] < 10) {
                 itemQuantities[position]++
                 binding.catItemQuantity.text = itemQuantities[position].toString()
             }
         }
-        private fun decreaseQuantity(position: Int){
-            if (itemQuantities[position]>1){
+
+        private fun decreaseQuantity(position: Int) {
+            if (itemQuantities[position] > 1) {
                 itemQuantities[position]--
                 binding.catItemQuantity.text = itemQuantities[position].toString()
             }
         }
-        private  fun deleteItem(position: Int){
+
+        private fun deleteItem(position: Int) {
             CartItems.removeAt(position)
             CartImage.removeAt(position)
             CartItemPrice.removeAt(position)
             notifyItemRemoved(position)
-            notifyItemRangeRemoved(position,CartItems.size)
+            notifyItemRangeRemoved(position, CartItems.size)
         }
     }
 }
