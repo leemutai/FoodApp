669b44193d2f8f72d9c3bbb00d635d09b704ee16
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-02-06 18:24:19 +0300

    created recentbuyadapter

--- app/src/main/java/com/example/wavesoffood/recentOrderItems.kt
+++ app/src/main/java/com/example/wavesoffood/recentOrderItems.kt
@@ -2,10 +2,40 @@ package com.example.wavesoffood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wavesoffood.databinding.ActivityRecentOrderItemsBinding
+import com.example.wavesoffood.model.OrderDetails
 
 class recentOrderItems : AppCompatActivity() {
+    private val binding :ActivityRecentOrderItemsBinding by lazy {
+        ActivityRecentOrderItemsBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recent_order_items)
+        setContentView(binding.root)
+
+        val recentOrderItems = intent.getParcelableArrayListExtra<OrderDetails>("RecentBuyOrderItem")
+        recentOrderItems?.let {orderDetails ->
+            if (orderDetails.isNotEmpty()){
+                val recentOrderItems = OrderDetails[0]
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
